a0755fd44c9adeb92cf77e14d6d1501264f19878
fwRelik <relik.fw@gmail.com>
2023-04-21 19:05:18 +0300

    Cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,12 @@ async function bootstrap() {
 	const port = process.env.PORT || 3000;
 
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 	app.setGlobalPrefix('api');
+
 	await app.listen(port);
 
 	Logger.log(`Server started: http://localhost:${port}`);
 }
+
 bootstrap();
